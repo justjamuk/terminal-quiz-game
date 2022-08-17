92ea94c767a7d02b6d564785528f16fcb4e332d9
Justjam <justjamuk@live.co.uk>
2022-08-17 14:23:00 +0100

    added questions

--- quiz_game.py
+++ quiz_game.py
@@ -1,10 +1,50 @@
+
+
 print("Welcome to my general knowledge quiz!")
 
 playing = input("Do you want to play? ")
 
-if playing != "yes":
+if playing.lower() != "yes":
     quit()
 
 print("okay! Let's Play!")
 
 score = 0 
+
+# lower() added so all answers are in lowercase. after each question if the answer is correct. 1 will be added to score.
+
+answer = input("How many bones does an adult human have? ")
+if answer.lower() == "206":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+    
+answer = input("Who is the worlds fastest man? ")
+if answer.lower() == "usain bolt":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("How many weeks n a year? ")
+if answer.lower() == "52":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("What is the capital of Spain? ")
+if answer.lower() == "madrid":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
+answer = input("How many birthdays are there? ")
+if answer.lower() == "366":
+    print("Correct!")
+    score += 1
+else:
+    print("Incorrect!")
+
